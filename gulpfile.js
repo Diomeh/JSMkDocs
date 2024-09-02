9aabaa5af0ae360e5bb9d0564686550b6b3d4c40
David Urbina <davidurbina.dev@gmail.com>
2024-09-02 17:04:43 -0500

    chore: fixed gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,21 @@
-import babel from 'gulp-babel';
-import gulp from 'gulp';
-import fs from "fs";
+const babel = require('gulp-babel');
+const {dest, series, src, watch} = require('gulp');
+const fs = require('fs');
 
-gulp.task('clean', () => fs.rmSync('./bin', { recursive: true, force: true }));
+const cleanTask = async () => fs.rmSync('./bin', {recursive: true, force: true});
 
-gulp.task('build', ['clean'], function () {
-	return gulp
-		.src('./src/**/*.js')
-		.pipe(
-			babel({
-				presets: ['es2015'],
-			})
-		)
-		.on('error', function (error) {
-			console.log(error.toString());
-			this.emit('end');
-		})
-		.pipe(gulp.dest('./bin'));
-});
+const buildTask = () => src('./src/**/*.js')
+	.pipe(babel({
+		presets: ['@babel/env'],
+	}))
+	.on('error', function (error) {
+		console.log(error.toString());
+		this.emit('end');
+	})
+	.pipe(dest('./bin'));
 
-gulp.task('watch', ['build'], function () {
-	gulp.watch('./src/**/*.js', ['build']);
-});
+const watchTask = () => watch('./src/**/*.js', series(cleanTask, buildTask));
+
+exports.clean = cleanTask;
+exports.build = series(cleanTask, buildTask);
+exports.watch = watchTask;
